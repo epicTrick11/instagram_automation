7d13fd7a5f10f9b606737ea2ba1e9c4a58097464
epicTrick11 <chandilayush2@gmail.com>
2025-06-08 08:26:38 +0530

    Create DeveloperUtils.kt

---
+++ app/patches/DeveloperUtils.kt
@@ -0,0 +1,14 @@
+package com.instella.android
+
+import android.content.Context
+import com.instagram.debug.devoptions.api.DeveloperOptionsLauncher
+import com.instagram.mainactivity.InstagramMainActivity
+
+
+class DeveloperUtils {
+    companion object {
+        fun openDeveloperMode(ctx: Context, instagramMainActivity: InstagramMainActivity) {
+            DeveloperOptionsLauncher.INSTANCE.loadAndLaunchDeveloperOptions(ctx, instagramMainActivity, instagramMainActivity.A05)
+        }
+    }
+}
